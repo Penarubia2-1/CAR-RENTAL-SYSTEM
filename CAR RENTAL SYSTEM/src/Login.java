09b7965558beaf6a45dceac8b8ebf98f6482f519
Imperial 2-1 <166680744+Jurieimperial@users.noreply.github.com>
2024-05-27 14:16:58 +0800

    Update Login.java

--- CAR RENTAL SYSTEM/src/Login.java
+++ CAR RENTAL SYSTEM/src/Login.java
@@ -71,11 +71,7 @@ public class Login implements ActionListener{
      
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-   
+     
 
     
     
